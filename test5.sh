547b668a0dd45dbb71e4f75e9a24ccc0254e998f
Parsifal <zmw@izmw.me>
2018-07-31 22:28:15 +0800

    update.

--- test5.sh
+++ test5.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-getFilePaths () {
+
+#判断文件类型是否为 png
+detectPngFile(){
+	# type = $(file -b $1 | cut -d, -f1)
+	# echo $type
+	echo $1
+}
+
+#递归遍历当前目录所有文件
+getFilePaths(){
 	dir=$(ls)
 
 	for i in $dir 
@@ -7,8 +16,10 @@ getFilePaths () {
 		if [[ -d "$i" ]]; then
 			cd "$i"
 			getFilePaths
+			cd ..
+		else
+			detectPngFile $i
 		fi
-		echo $i
 	done
 }
 
